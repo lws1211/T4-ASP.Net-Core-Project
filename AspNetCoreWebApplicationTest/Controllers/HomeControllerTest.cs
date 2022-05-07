22171b9327f5b7d86ea4f80918958eef22581a86
Wan Seng Liew (NCS) <wanseng.liew@ncs.com.sg>
2022-05-07 10:20:01 +0800

    whatever

--- AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
+++ AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
@@ -7,6 +7,13 @@ namespace AspNetCoreWebApplicationTest.Controllers
     public class HomeControllerTest
     {
         [Fact]
+        public void NoInputParamGetResponseTest()
+        {
+            HelloController controller = new HelloController();
+            var response = controller.Get().Value as Response;
+            Assert.Equal("Hello World!!!", response.output);
+        }
+        
         public void IndexTest()
         {
             HomeController controller = new HomeController();
